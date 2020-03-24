4260f83bc09ab2f10eec7f2176e4094e25da2287
Kadir AKYIL <kadirakyil@msn.com>
2020-03-24 09:47:47 +0300

    First Push

--- EksiogluBirlik_Web/js/main.js
+++ EksiogluBirlik_Web/js/main.js
@@ -1,16 +1,21 @@
  AOS.init({
  	duration: 800,
  	easing: 'slide',
- 	once: false
+ 	once: true
  });
+
+
+// $(window).load(function() {
+	
+// })
 
 jQuery(document).ready(function($) {
 
 	"use strict";
 
-	// $(".loader").delay(1000).fadeOut("slow");
- //  $("#overlayer").delay(1000).fadeOut("slow");	
-
+	$(".loader").delay(1000).fadeOut("slow");
+  $("#overlayer").delay(1000).fadeOut("slow");	
+	
 	var siteMenuClone = function() {
 
 		$('.js-clone-nav').each(function() {
@@ -124,7 +129,39 @@ jQuery(document).ready(function($) {
 	// siteSliderRange();
 
 
-	
+	var siteMagnificPopup = function() {
+		$('.image-popup').magnificPopup({
+	    type: 'image',
+	    closeOnContentClick: true,
+	    closeBtnInside: false,
+	    fixedContentPos: true,
+	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+	     gallery: {
+	      enabled: true,
+	      navigateByImgClick: true,
+	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	    },
+	    image: {
+	      verticalFit: true
+	    },
+	    zoom: {
+	      enabled: true,
+	      duration: 300 // don't foget to change the duration also in CSS
+	    }
+	  });
+
+	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
+
+	    fixedContentPos: false
+	  });
+	};
+	siteMagnificPopup();
+
 
 	var siteCarousel = function () {
 		if ( $('.nonloop-block-13').length > 0 ) {
@@ -133,197 +170,102 @@ jQuery(document).ready(function($) {
 		    items: 1,
 		    loop: true,
 				stagePadding: 0,
-		    margin: 0,
-		    smartSpeed: 1000,
+		    margin: 20,
 		    autoplay: true,
+		    autoHeight: true,
 		    nav: true,
 				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
 		    responsive:{
 	        600:{
 	        	margin: 0,
-	        	nav: true,
-	          items: 2
+	        	stagePadding: 10,
+	          items: 1
 	        },
 	        1000:{
 	        	margin: 0,
 	        	stagePadding: 0,
-	        	nav: true,
-	          items: 2
+	          items: 1
 	        },
 	        1200:{
 	        	margin: 0,
 	        	stagePadding: 0,
-	        	nav: true,
-	          items: 3
+	          items: 1
 	        }
 		    }
 			});
 		}
 
-		let owl2 = $('.slide-one-item-alt-text').owlCarousel({
+		$('.nonloop-block-13').owlCarousel({
 	    center: false,
 	    items: 1,
 	    loop: true,
+	    autoplay: true,
 			stagePadding: 0,
-	    margin: 0,
-	    smartSpeed: 1000,
-	    autoplay: true,
-	    pauseOnHover: true,
-	    onDragged: function(event) {
-	    	console.log('event : ',event.relatedTarget['_drag']['direction'])
-	    	if ( event.relatedTarget['_drag']['direction'] == 'left') {
-	    		$('.owl-1').trigger('next.owl.carousel');
-	    	} else {
-	    		$('.owl-1').trigger('prev.owl.carousel');
-	    	}
+	    margin: 20,
+	    nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+	    responsive:{
+        600:{
+        	margin: 0,
+        	stagePadding: 0,
+          items: 2
+        },
+        1000:{
+        	margin: 0,
+        	stagePadding: 0,
+          items: 2
+        },
+        1200:{
+        	margin: 0,
+        	stagePadding: 0,
+          items: 3
+        }
 	    }
-	  });
-
-		let owl = $('.owl-1').owlCarousel({
-			// animateOut: 'fadeOut',
-			center: true,
-			items: 1,
-			loop: true,
-			margin: 0,
-			smartSpeed: 1500,
-			dots: true,
-	    autoplay: true,
-	    pauseOnHover: false,
-	    onDragged: function(event) {
-	    	console.log('event : ',event.relatedTarget['_drag']['direction'])
-	    	if ( event.relatedTarget['_drag']['direction'] == 'left') {
-	    		$('.slide-one-item-alt-text').trigger('next.owl.carousel');
-	    	} else {
-	    		$('.slide-one-item-alt-text').trigger('prev.owl.carousel');
-	    	}
-	    }
-		})
-
-		$( '.owl-dot' ).on( 'click', function() {
-		  console.log(owl2.trigger('to.owl.carousel', $(this).index()));
-		})
-
-
-
-
-		$('.owl-2').owlCarousel({
-			animateOut: 'fadeOut',
-			center: true,
-			items: 1,
-			loop: true,
-			margin: 0,
-			smartSpeed: 1500,
-	    autoplay: true,
-	    pauseOnHover: false
 		});
-		$('.owl-3').owlCarousel({
-			animateOut: 'fadeOut',
-			center: true,
-			items: 1,
-			loop: true,
-			margin: 0,
-			smartSpeed: 1500,
-	    autoplay: true,
-	    pauseOnHover: false
-		})
 
-		$('.slide-one-item').owlCarousel({
-	    center: false,
-	    items: 1,
-	    loop: true,
-			stagePadding: 0,
-	    margin: 0,
-	    smartSpeed: 1500,
-	    autoplay: true,
-	    pauseOnHover: false,
-	    dots: true,
-	    nav: false,
-	    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-	  });
-
-
-	  
-
-	  $('.slide-one-item-alt').owlCarousel({
-	    center: false,
-	    items: 1,
-	    loop: true,
-			stagePadding: 0,
-	    margin: 0,
-	    smartSpeed: 1000,
-	    autoplay: true,
-	    pauseOnHover: true,
-	    onDragged: function(event) {
-	    	console.log('event : ',event.relatedTarget['_drag']['direction'])
-	    	if ( event.relatedTarget['_drag']['direction'] == 'left') {
-	    		$('.slide-one-item-alt-text').trigger('next.owl.carousel');
-	    	} else {
-	    		$('.slide-one-item-alt-text').trigger('prev.owl.carousel');
-	    	}
-	    }
-	  });
-
-	  if ( $('.owl-all').length > 0 ) {
-			$('.owl-all').owlCarousel({
+		if ( $('.slide-one-item').length > 0 ) {
+			$('.slide-one-item').owlCarousel({
 		    center: false,
 		    items: 1,
-		    loop: false,
+		    loop: true,
 				stagePadding: 0,
 		    margin: 0,
-		    autoplay: false,
-		    nav: false,
-		    dots: true,
-		    touchDrag: true,
-  			mouseDrag: true,
-  			smartSpeed: 1000,
-				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-		    responsive:{
-	        768:{
-	        	margin: 30,
-	        	nav: false,
-	        	responsiveRefreshRate: 10,
-	          items: 1
-	        },
-	        992:{
-	        	margin: 30,
-	        	stagePadding: 0,
-	        	nav: false,
-	        	responsiveRefreshRate: 10,
-	        	touchDrag: false,
-  					mouseDrag: false,
-	          items: 3
-	        },
-	        1200:{
-	        	margin: 30,
-	        	stagePadding: 0,
-	        	nav: false,
-	        	responsiveRefreshRate: 10,
-	        	touchDrag: false,
-  					mouseDrag: false,
-	          items: 3
-	        }
-		    }
-			});
-		}
-		
+		    autoplay: true,
+		    pauseOnHover: false,
+		    nav: true,
+		    navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
+		  });
+	  }
 	};
 	siteCarousel();
 
-	
+	var siteStellar = function() {
+		$(window).stellar({
+	    responsive: false,
+	    parallaxBackgrounds: true,
+	    parallaxElements: true,
+	    horizontalScrolling: false,
+	    hideDistantElements: false,
+	    scrollProperty: 'scroll'
+	  });
+	};
+	// siteStellar();
 
 	var siteCountDown = function() {
 
-		$('#date-countdown').countdown('2020/10/10', function(event) {
-		  var $this = $(this).html(event.strftime(''
-		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
-		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
-		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
-		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
-		});
+		if ( $('#date-countdown').length > 0 ) {
+			$('#date-countdown').countdown('2020/10/10', function(event) {
+			  var $this = $(this).html(event.strftime(''
+			    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+			    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+			    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+			    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+			    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+			});
+		}
 				
 	};
-	// siteCountDown();
+	siteCountDown();
 
 	var siteDatePicker = function() {
 
@@ -332,27 +274,27 @@ jQuery(document).ready(function($) {
 		}
 
 	};
-	// siteDatePicker();
+	siteDatePicker();
 
 	var siteSticky = function() {
-		$(".js-sticky-header").sticky({topSpacing:0});
+		if ( $(".js-sticky-header").length > 0 ) {
+			$(".js-sticky-header").sticky({topSpacing:0});
+		}
 	};
 	siteSticky();
 
 	// navigation
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
-
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a[href^='#']", function(e) {
+   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
       e.preventDefault();
 
       var hash = this.hash;
 
       $('html, body').animate({
-        'scrollTop': $(hash).offset().top - 50
-      }, 600, 'easeInOutExpo', function() {
-        // window.location.hash = hash;
-
+        'scrollTop': $(hash).offset().top - 0
+      }, 1000, 'easeInOutCirc', function(){
+        window.location.hash = hash;
       });
 
     });
@@ -360,8 +302,6 @@ jQuery(document).ready(function($) {
   OnePageNavigation();
 
   var siteScroll = function() {
-
-  	
 
   	$(window).scroll(function() {
 
@@ -377,33 +317,5 @@ jQuery(document).ready(function($) {
 
   };
   siteScroll();
-
-
-  var counter = function() {
-		
-		$('#about-section').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number > span').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
-	}
-	counter();
-
-
 
 });
